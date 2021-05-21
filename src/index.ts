38fab5088d3faa58662a69c5b74305db4918ee9a
YouRa <allakhverdovyuriy@gmail.com>
2021-05-21 09:47:08 +0300

    Add NumbersCollection class

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-    
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i -1; j++) {
-
-        // If collection is an array of numbers
-        if (this.collection instanceof Array) {
-          // colection has type of number[]
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        // If collection is a string
-        if (typeof this.collection === 'string') {
-          
-        }
-      
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([4, -3, 11, 2]);
+const numbersCollection = new NumbersCollection([4, -300, 11, 2]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
