acba6422a7a9777dccba64587e5f1e7f33aad497
YouRa <allakhverdovyuriy@gmail.com>
2021-05-21 10:53:30 +0300

    Add Sortable interface

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([4, -300, 11, 2]);
+var numbersCollection = new NumbersCollection_1.NumbersCollection([4, -300, 110, 2]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([4, -300, 11, 2]);
+const numbersCollection = new NumbersCollection([4, -300, 110, 2]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
