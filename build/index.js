8ac65fe41e6685d298bf54ed2c19f887d23b4d7e
YouRa <allakhverdovyuriy@gmail.com>
2021-05-21 11:35:53 +0300

    Add CharactersCollection class

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([4, -300, 110, 2]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('ZxYmba');
+var sorter1 = new Sorter_1.Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection.data);
